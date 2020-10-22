ddcdbec21582fcb2a17876ad63eb72171f9e7ea3
Kuddleman <edl1155@gmail.com>
2020-10-21 21:08:52 -0700

    add gitignore and connect to mongo database

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag')
+const mongoose = require('mongoose')
+
+const { MONGODB } = require('./config')
+
 const typeDefs = gql `
    type Query {
      sayHi: String!
@@ -17,7 +21,16 @@ const server = new ApolloServer({
   resolvers
 })
 
-server.listen({ port: 5000 })
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log('MongoDB Connected!')
+    return server.listen({ port: 5000 })
+  })
   .then( res => {
     console.log(`Server running at ${ res.url }`)
   })
+  
+
+
+  
